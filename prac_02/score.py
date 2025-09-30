1412854038214b58981122e55ee864ffcd5ab309
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-09-30 15:21:37 +1000

    Refractor practical 1 score.py code and add random score determiner

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,23 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_score(score))
+
+    random_score = random.uniform(0,100)
+    print(f"Random score is {random_score:.2f} which is {determine_score(random_score)}")
+
+def determine_score(score: float):
+    if score < 0 or score > 100:
+        return "invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()

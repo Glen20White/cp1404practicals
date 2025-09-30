76d699b1ac4b8b63bb155787a5d3d824d571aa32
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-09-30 14:50:18 +1000

    Refractor password check program to use functions

---
+++ prac_02/password_stars.py
@@ -0,0 +1,19 @@
+def main():
+    minimum_length = 6
+    password = get_password(minimum_length)
+    print_censored_password(password)
+
+
+def print_censored_password(password):
+    print('*' * len(password))
+
+
+def get_password(minimum_length):
+    password = input(f"Please enter your password ensuring it is at least {minimum_length} characters long: ")
+    while len(password) < minimum_length:
+        print(f"Password is not at least {minimum_length} characters long")
+        password = input(f"Please enter your password ensuring it is at least {minimum_length} characters long: ")
+    return password
+
+
+main()

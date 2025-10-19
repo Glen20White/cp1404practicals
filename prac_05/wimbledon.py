15501aeb58c7213e33690a528c5d39cb795a0f92
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-10-20 00:01:04 +1000

    Complete prac 5 Wimbledon exersize

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,39 @@
+FILENAME = "wimbledon.csv"
+
+def main():
+    champions, countries = read_wimbledon_data(FILENAME)
+    display_champions(champions)
+    display_countries(countries)
+
+
+def read_wimbledon_data(filename):
+    champions_to_wins = {}
+    countries = set()
+
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            data = line.strip().split(",")
+            champion = data[2]
+            country = data[1]
+            if champion in champions_to_wins:
+                champions_to_wins[champion] += 1
+            else:
+                champions_to_wins[champion] = 1
+            countries.add(country)
+    return champions_to_wins, sorted(countries)
+
+
+def display_champions(champions):
+    print("Wimbledon Champions:")
+    for champion, wins in champions.items():
+        print(f"{champion} {wins}")
+
+
+def display_countries(countries):
+    print("\nThese countries have won Wimbledon:")
+    countries_string = ", ".join(countries)
+    print(countries_string)
+
+
+main()

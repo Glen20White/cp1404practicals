773234d35ec84c7096e3488218c6ff955d58397f
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-10-19 17:11:49 +1000

    Complete prac 5 word occurrences

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 Practical 5
+Question 2
+Word Occurrences
+Estimate: 15 minutes
+Actual:   15 minutes
+"""
+
+word_to_count = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    frequency = word_to_count.get(word, 0)
+    word_to_count[word] = frequency + 1
+
+max_length = max((len(word) for word in words))
+for word in sorted(word_to_count):
+    print(f"{word:{max_length}} : {word_to_count[word]}")

53573b3eb65736a7261969e46cffb5bfd579d424
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-11-09 20:56:48 +1000

    Complete More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,7 +2,12 @@ from guitar import Guitar
 
 def main():
     guitars = load_guitars("guitars.csv")
-    sort_guitars(guitars)
+    print("These are the existing guitars:")
+    display_guitars(guitars)
+    add_new_guitars(guitars)
+    guitars.sort()
+    save_guitars("guitars.csv", guitars)
+    print("Guitars have been saved to guitars.csv")
 
 
 def load_guitars(filename):
@@ -22,5 +27,32 @@ def sort_guitars(guitars):
         print(guitar)
 
 
-if __name__ == "__main__":
-    main()
+def display_guitars(guitars):
+    """Display guitars in a formatted list with vintage identification."""
+    print("\nGuitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar}{vintage}")
+
+
+def add_new_guitars(guitars):
+    """Add new guitars to the csv file."""
+    print("Add a guitar")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(f"{guitar_to_add} added.")
+        name = input("Name: ")
+
+
+def save_guitars(filename, guitars):
+    """Write new guitars to the CSV file."""
+    with open(filename, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
+
+main()

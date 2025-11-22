35cf1e4af06a5502cfe89419ec5c0e3f87f53263
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-11-22 17:22:43 +1000

    Edit format and comments

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ from prac_09.taxi import Taxi
 def main():
     """Test Taxi class."""
     my_taxi = Taxi("Prius 1", 100)    # 1. New taxi object, named Prius 1, contains 100 units of fuel, (at a price of $1.23/km (moved to taxi class as a class variable))
-    my_taxi.drive(40)                       # 2. Drive the taxi 40 km
+    my_taxi.drive(40)                 # 2. Drive the taxi 40 km
     # 3. Print the taxi's details and the current fare
     print(my_taxi)
     print(f"Current fair is ${my_taxi.get_fare()}")

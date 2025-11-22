e368f9458838ebdc1a6afad48e03996dbae6cc1c
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-11-22 18:17:02 +1000

    Create new code with a taxi subclass that introduces a fanciness factor

---
+++ prac_09/silver_service.py
@@ -0,0 +1,19 @@
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """Represent a SilverServiceTaxi class, a subclass of Taxi."""
+    flagfall = 4.50 #new fair charge
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialize a SilverServiceTaxi instance, based on the parent class Taxi."""
+        super().__init__(name,fuel)
+        self.fanciness = fanciness
+        self.price_per_km *=fanciness
+
+    def __str__(self):
+        """Return a string representation of the SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall} "
+
+    def get_fare(self):
+        """Return the total price for the taxi trip"""
+        return super().get_fare() + self.flagfall

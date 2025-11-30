aa9facab8652f72f4813c1173baac997f0ce50ff
Glen20White <110080921+Glen20White@users.noreply.github.com>
2025-11-30 19:13:21 +1000

    Remove auto suggest, add error handling for unknown pages and add suggestions for similar searches, close program when nothing is entered

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,9 +3,19 @@ import wikipedia
 def main():
     while True:
         page_title_input = input("Enter page title: ")
-        page = wikipedia.page(page_title_input)
-        print(page.title)
-        print(page.summary)
-        print(page.url)
+        if page_title_input == "":
+            print("Thank you")
+            break
+
+        try:
+            page = wikipedia.page(page_title_input, auto_suggest=False)
+            print(page.title)
+            print(page.summary)
+            print(page.url)
+
+        except wikipedia.DisambiguationError as e:
+            print("We need a more specific title. Try one of the following, or a new search:", e.options)
+        except wikipedia.PageError as e:
+            print(f"Page id '{page_title_input}' does not match any pages. Try another id!")
 
 main()
